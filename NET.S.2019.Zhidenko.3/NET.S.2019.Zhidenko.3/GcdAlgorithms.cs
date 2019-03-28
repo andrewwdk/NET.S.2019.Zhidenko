2ed060aa7042141df012329386680a6f16187b86
Andrei Zhidenko <zhidenko_1999@mail.ru>
2019-03-29 02:41:34 +0300

    Implementation of Euclidean GCD algorythm

---
+++ NET.S.2019.Zhidenko.3/NET.S.2019.Zhidenko.3/GcdAlgorithms.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NET.S._2019.Zhidenko._3
+{
+    public class GcdAlgorythms
+    {
+        public static int EuclideanGcd(params int[] numbers)
+        {
+            if (numbers == null)
+                throw new ArgumentNullException();
+
+            if (numbers.Length == 0 || numbers.Length == 1)
+                throw new ArgumentException();
+
+            int curGcd = SimpleEuclideanGcd(numbers[0], numbers[1]);
+
+            for (int i = 2; i < numbers.Length; i++)
+                curGcd = SimpleEuclideanGcd(curGcd, numbers[i]);
+
+            return curGcd;
+        }
+
+        public static int EuclideanGcdWithTime(out long time, params int[] numbers)
+        {
+            Stopwatch timer = new Stopwatch();
+
+            EuclideanGcd(numbers); //get result without time wasted on compilation
+
+            timer.Start();
+            int result = EuclideanGcd(numbers);
+            timer.Stop();
+            time = timer.ElapsedMilliseconds;
+
+            return result;
+
+        }
+
+        private static int SimpleEuclideanGcd(int a, int b)
+        {
+            a = Math.Abs(a);
+            b = Math.Abs(b);
+
+            if (a == 0)
+                return b;
+
+            if (b == 0)
+                return a;
+
+            while (a != b)
+            {
+                if (a > b)
+                {
+                    a -= b;
+                }
+                else
+                {
+                    b -= a;
+                }
+            }
+
+            return a;
+        }
+    }
+}
---
+++ NET.S.2019.Zhidenko.3/NUnitTests/GcdAlgorythmsTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using NUnit.Framework;
+using NET.S._2019.Zhidenko._3;
+
+namespace NUnitTests
+{
+    [TestFixture]
+    public class GcdAlgorythmsTests
+    {
+        [TestCase(0, 0, ExpectedResult = 0)]
+        [TestCase(0, 0, 0, 0, ExpectedResult = 0)]
+        [TestCase(1, 10, ExpectedResult = 1)]
+        [TestCase(1, 10, 5, ExpectedResult = 1)]
+        [TestCase(5, 5, ExpectedResult = 5)]
+        [TestCase(5, 10, ExpectedResult = 5)]
+        [TestCase(5, 10, 15, 20, 25, 30, ExpectedResult = 5)]
+        [TestCase(0, 5, ExpectedResult = 5)]
+        [TestCase(5, 5, 0, ExpectedResult = 5)]
+        [TestCase(15, 0, 5, ExpectedResult = 5)]
+        [TestCase(-5, 5, 0, ExpectedResult = 5)]
+        [TestCase(-5, 5, -5, ExpectedResult = 5)]
+        public int EuclideanGcd_with_correct_data(params int[] numbers)
+        {
+            return GcdAlgorythms.EuclideanGcd(numbers);
+        }
+    }
+}
