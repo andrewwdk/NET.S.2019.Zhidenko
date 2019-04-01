1c18b1908bc6ee503e0688200ad3bd69665c3e90
Andrei Zhidenko <zhidenko_1999@mail.ru>
2019-04-01 13:06:09 +0300

    Add StyleCop

--- NET.S.2019.Zhidenko.5/NET.S.2019.Zhidenko.5/BubbleSort.cs
+++ NET.S.2019.Zhidenko.5/NET.S.2019.Zhidenko.5/BubbleSort.cs
@@ -1,11 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace NET.S._2019.Zhidenko._5
 {
+    /// <summary> Define type of sort: increasing or decreasing </summary>
     public enum SortType
     {
         increasing,
@@ -15,123 +12,163 @@ namespace NET.S._2019.Zhidenko._5
     public class BubbleSort
     {
         /// <summary> Sorts array of arrays by sum of elements in each array </summary>
-        /// <param name="array">Array</param>
+        /// <param name="array">Array to sort by sum</param>
         /// <param name="type">Type of sort: increasing or decreasing</param>
         public static void SortBySum(int[][] array, SortType type)
         {
             if (array == null)
+            {
                 throw new ArgumentNullException();
+            }
 
             if (array.Length == 0)
+            {
                 throw new ArgumentException();
+            }
 
-            for(int i = 0; i < array.Length - 1; i++)
-                for(int j = i + 1; j < array.Length; j++)
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                for (int j = i + 1; j < array.Length; j++)
                 {
-                    if(type == SortType.increasing)
+                    if (type == SortType.increasing)
                     {
                         if (GetSum(array[j]) < GetSum(array[i]))
+                        {
                             SwapArrays(ref array[i], ref array[j]);
+                        }
                     }
                     else
                     {
                         if (GetSum(array[j]) > GetSum(array[i]))
+                        {
                             SwapArrays(ref array[i], ref array[j]);
+                        }
                     }
                 }
+            }
         }
 
         /// <summary> Sorts array of arrays by max element in each array </summary>
-        /// <param name="array">Array</param>
+        /// <param name="array">Array to sort by max element</param>
         /// <param name="type">Type of sort: increasing or decreasing</param>
         public static void SortByMaxElement(int[][] array, SortType type)
         {
             if (array == null)
+            {
                 throw new ArgumentNullException();
+            }
 
             if (array.Length == 0)
+            {
                 throw new ArgumentException();
+            }
 
             for (int i = 0; i < array.Length - 1; i++)
+            {
                 for (int j = i + 1; j < array.Length; j++)
                 {
                     if (type == SortType.increasing)
                     {
                         if (FindMaxElement(array[j]) < FindMaxElement(array[i]))
+                        {
                             SwapArrays(ref array[i], ref array[j]);
+                        }
                     }
                     else
                     {
                         if (FindMaxElement(array[j]) > FindMaxElement(array[i]))
+                        {
                             SwapArrays(ref array[i], ref array[j]);
+                        }
                     }
                 }
+            }
         }
 
         /// <summary> Sorts array of arrays by min element in each array </summary>
-        /// <param name="array">Array</param>
+        /// <param name="array">Array to sort by min element</param>
         /// <param name="type">Type of sort: increasing or decreasing</param>
         public static void SortByMinElement(int[][] array, SortType type)
         {
             if (array == null)
+            {
                 throw new ArgumentNullException();
+            }
 
             if (array.Length == 0)
+            {
                 throw new ArgumentException();
+            }
 
             for (int i = 0; i < array.Length - 1; i++)
+            {
                 for (int j = i + 1; j < array.Length; j++)
                 {
                     if (type == SortType.increasing)
                     {
                         if (FindMinElement(array[j]) < FindMinElement(array[i]))
+                        {
                             SwapArrays(ref array[i], ref array[j]);
+                        }
                     }
                     else
                     {
                         if (FindMinElement(array[j]) > FindMinElement(array[i]))
+                        {
                             SwapArrays(ref array[i], ref array[j]);
+                        }
                     }
                 }
+            }
         }
 
         /// <summary> Finds sum of all elements in the array </summary>
-        /// <param name="array">Array</param>
+        /// <param name="array">Array to find sum</param>
         /// <returns>Sum of elements</returns>
         private static int GetSum(int[] array)
         {
             int sum = 0;
 
             foreach (int number in array)
+            {
                 sum += number;
+            }
 
             return sum;
         }
 
         /// <summary> Finds max element in the array </summary>
-        /// <param name="array">Array</param>
+        /// <param name="array">Array to find max element</param>
         /// <returns>Max element</returns>
         private static int FindMaxElement(int[] array)
         {
             int maxElement = int.MinValue;
 
             foreach (int number in array)
+            {
                 if (number > maxElement)
+                {
                     maxElement = number;
+                }
+            }
 
             return maxElement;
         }
 
-        /// <summary> Finds min element in the array </summary>
-        /// <param name="array">Array</param>
+        /// <summary>Finds min element in the array</summary>
+        /// <param name="array">Array to find min element</param>
         /// <returns>Min element</returns>
         private static int FindMinElement(int[] array)
         {
             int minElement = int.MaxValue;
 
             foreach (int number in array)
+            {
                 if (number < minElement)
+                {
                     minElement = number;
+                }
+            }
 
             return minElement;
         }
--- NET.S.2019.Zhidenko.5/NET.S.2019.Zhidenko.5/Polynomial.cs
+++ NET.S.2019.Zhidenko.5/NET.S.2019.Zhidenko.5/Polynomial.cs
@@ -1,242 +1,163 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace NET.S._2019.Zhidenko._5
 {
+    /// <summary>Class for polynomials</summary>
     public class Polynomial
     {
+        /// <summary>Coefficients of polynomial</summary>
         private double[] coefs;
 
-          /// <summary>
-          /// Constructor which create a polinom
-          /// </summary>
-          /// <param name="coef"> Coefficients for polinom</param>
+        /// <summary>Initializes a new instance of the Polynomial class</summary>
+        /// <param name="coefs"> Coefficients for polynomial</param>
         public Polynomial(double[] coefs)
         {
             if (coefs == null)
+            {
                 throw new ArgumentNullException();
-
-            /*if (coefs.Length == 0)
-                throw new ArgumentException();*/
+            }
 
             this.coefs = new double[coefs.Length];
 
             for (int i = 0; i < coefs.Length; i++)
+            {
                 this.coefs[i] = coefs[i];
+            }
         }
 
-        /// <summary>
-        /// Count of coefs
-        /// </summary>
-        public int Length { get { return coefs.Length; } }
+        /// <summary>Gets count of coefficients</summary>
+        public int Length
+        {
+            get
+            {
+                return this.coefs.Length;
+            }
+        }
 
-        /// <summary>
-        /// Accesor to coefs
-        /// </summary>
-        /// <param name="index"> Index of needed coef</param>
-        /// <returns> Coef with certain index </returns>
+        /// <summary>Accessor to coefficients</summary>
+        /// <param name="index"> Index of needed coefficient</param>
+        /// <returns> Coefficient with certain index </returns>
         public double this[int index]
         {
             get
             {
-                if (index < 0 || index > coefs.Length - 1)
+                if (index < 0 || index > this.coefs.Length - 1)
+                {
                     throw new IndexOutOfRangeException();
+                }
 
-                return coefs[index];
+                return this.coefs[index];
             }
         }
 
-        /// <summary>
-        /// Calculate result of polinom with certain X
-        /// </summary>
-        /// <param name="value"> Value of X</param>
-        /// <returns> Result of polinom </returns>
-        public double GetResult(double value)
+        /// <summary> Add one polynomial to another</summary>
+        /// <param name="firstPolynom"> The first polynomial</param>
+        /// <param name="secondPolynom"> The second polynomial</param>
+        /// <returns> New polynomial which is sum of two polynomials </returns>
+        public static Polynomial operator +(Polynomial firstPolynom, Polynomial secondPolynom)
         {
-            double result = 0;
-
-            for (int i = coefs.Length - 1; i >= 0; i--)
-                result += coefs[i] * Math.Pow(value, i);
-
-            return result;
-        }
-
-        /// <summary>
-        /// Transform polinom to a string
-        /// </summary>
-        /// <returns> Polinomial sting</returns>
-        public override string ToString()
-        {
-            string result = "";
-
-            for(int i = 0; i < coefs.Length; i++)
+            if (firstPolynom == null && secondPolynom == null)
             {
-                if (coefs[i] == 0)
-                    continue;
-
-                if (result == "")
-                {
-                    if (Math.Abs(coefs[i]) != 1)
-                    {
-                        result += coefs[0].ToString();
-                    }
-                    else if (coefs[i] == -1)
-                        result += "-";            
-                }
-                else
-                {
-                    result += (coefs[i] > 0) ? " + " : " - ";
-
-                    if (Math.Abs(coefs[i]) != 1 || i == coefs.Length - 1)
-                        result += Math.Abs(coefs[i]).ToString();
-                }
-
-                if (i != coefs.Length - 2 && i != coefs.Length - 1)
-                {
-                    result += ("x^" + (coefs.Length - i - 1).ToString());
-                }
-                else if (i == coefs.Length - 2)
-                    result += "x";
-            }
-            return result;
-        }
-
-        /// <summary>
-        /// Hash function
-        /// </summary>
-        /// <returns> Hash of polynom</returns>
-        public override int GetHashCode()
-        {
-            return coefs.GetHashCode();
-        }
-
-        /// <summary>
-        /// Compare two polynoms
-        /// </summary>
-        /// <param name="obj"> Polynom to compare</param>
-        /// <returns> Result of comparison </returns>
-        public override bool Equals(object obj)
-        {
-            Polynomial p = (Polynomial)obj;
-
-            if (p == null)
-                return false;
-
-            if (coefs.Length != p.Length)
-                return false;
-
-            for(int i = 0; i < coefs.Length; i++)
-            {
-                if (coefs[i] != p[i])
-                    return false;
-            }
-
-            return true;
-        }
-
-        /// <summary>
-        /// Add one polynom to another
-        /// </summary>
-        /// <param name="firstPolinom"> The first polynom</param>
-        /// <param name="secondPolinom"> The second polynom</param>
-        /// <returns> New polynom which is sum of two polynoms </returns>
-        public static Polynomial operator +(Polynomial firstPolinom, Polynomial secondPolinom)
-        {
-            if (firstPolinom == null && secondPolinom == null)
                 return null;
+            }
 
-            if (firstPolinom == null)
-                return secondPolinom;
-
-            if (secondPolinom == null)
-                return firstPolinom;
-
-            Polynomial smallerPolinom, biggerPolinom;
-
-            if(firstPolinom.Length < secondPolinom.Length)
+            if (firstPolynom == null)
             {
-                smallerPolinom = firstPolinom;
-                biggerPolinom = secondPolinom;
+                return secondPolynom;
+            }
+
+            if (secondPolynom == null)
+            {
+                return firstPolynom;
+            }
+
+            Polynomial smallerPolynom, biggerPolynom;
+
+            if (firstPolynom.Length < secondPolynom.Length)
+            {
+                smallerPolynom = firstPolynom;
+                biggerPolynom = secondPolynom;
             }
             else
             {
-                biggerPolinom = firstPolinom;
-                smallerPolinom = secondPolinom;
+                biggerPolynom = firstPolynom;
+                smallerPolynom = secondPolynom;
             }
 
-            double[] newArray = new double[biggerPolinom.Length];
+            double[] newArray = new double[biggerPolynom.Length];
 
-            int i = smallerPolinom.Length - 1, j = biggerPolinom.Length - 1;
-            for(; i >= 0; i--, j--)
+            int i = smallerPolynom.Length - 1, j = biggerPolynom.Length - 1;
+            for (; i >= 0; i--, j--)
             {
-                newArray[j] = smallerPolinom[i] + biggerPolinom[j];
+                newArray[j] = smallerPolynom[i] + biggerPolynom[j];
             }
 
             for (; j >= 0; j--)
             {
-                newArray[j] = biggerPolinom[j];
+                newArray[j] = biggerPolynom[j];
             }
 
             return new Polynomial(newArray);
         }
 
-        /// <summary>
-        /// Substruct one polynom from another
-        /// </summary>
-        /// <param name="firstPolinom"> The first polynom</param>
-        /// <param name="secondPolinom"> The second polynom</param>
-        /// <returns> New polynom which is substruction of two polynoms </returns>
-        public static Polynomial operator -(Polynomial firstPolinom, Polynomial secondPolinom)
+        /// <summary>Subtract one polynomial from another</summary>
+        /// <param name="firstPolynom"> The first polynomial</param>
+        /// <param name="secondPolynom"> The second polynomial</param>
+        /// <returns> New polynomial which is subtraction of two polynomials </returns>
+        public static Polynomial operator -(Polynomial firstPolynom, Polynomial secondPolynom)
         {
-            if (firstPolinom == null && secondPolinom == null)
+            if (firstPolynom == null && secondPolynom == null)
+            {
                 return null;
+            }
 
-            if (secondPolinom == null)
-                return firstPolinom;
+            if (secondPolynom == null)
+            {
+                return firstPolynom;
+            }
 
             double[] newCoefs;
 
-            if (firstPolinom == null)
+            if (firstPolynom == null)
             {
-                newCoefs = new double[secondPolinom.Length];
+                newCoefs = new double[secondPolynom.Length];
 
-                for (int i = 0; i < secondPolinom.Length; i++)
-                    newCoefs[i] = (-1) * secondPolinom[i];
+                for (int i = 0; i < secondPolynom.Length; i++)
+                {
+                    newCoefs[i] = (-1) * secondPolynom[i];
+                }
 
                 return new Polynomial(newCoefs);
             }
 
-            if(firstPolinom.Length >= secondPolinom.Length)
+            if (firstPolynom.Length >= secondPolynom.Length)
             {
-                int i = firstPolinom.Length - 1, j = secondPolinom.Length - 1;
-                newCoefs = new double[firstPolinom.Length];
+                int i = firstPolynom.Length - 1, j = secondPolynom.Length - 1;
+                newCoefs = new double[firstPolynom.Length];
 
                 for (; j >= 0; i--, j--)
                 {
-                    newCoefs[i] = firstPolinom[i] - secondPolinom[j];
+                    newCoefs[i] = firstPolynom[i] - secondPolynom[j];
                 }
 
                 for (; i >= 0; i--)
                 {
-                    newCoefs[i] = firstPolinom[i];
+                    newCoefs[i] = firstPolynom[i];
                 }
             }
             else
             {
-                int i = firstPolinom.Length - 1, j = secondPolinom.Length - 1;
-                newCoefs = new double[secondPolinom.Length];
+                int i = firstPolynom.Length - 1, j = secondPolynom.Length - 1;
+                newCoefs = new double[secondPolynom.Length];
 
                 for (; i >= 0; i--, j--)
                 {
-                    newCoefs[j] = firstPolinom[i] - secondPolinom[j];
+                    newCoefs[j] = firstPolynom[i] - secondPolynom[j];
                 }
 
                 for (; j >= 0; j--)
                 {
-                    newCoefs[j] = (-1) * secondPolinom[j];
+                    newCoefs[j] = (-1) * secondPolynom[j];
                 }
             }
 
@@ -245,67 +166,44 @@ namespace NET.S._2019.Zhidenko._5
             return new Polynomial(newCoefs);
         }
 
-        /// <summary>
-        /// Delete unneccesary members of polynom
-        /// </summary>
-        /// <param name="coefs">Initial coefficients</param>
-        /// <returns>Coefficients without 0 at the beggining</returns>
-        private static double[] DeleteZeros(double[] coefs)
+        /// <summary>Multiply one polynomial with another</summary>
+        /// <param name="firstPolynom"> The first polynomial</param>
+        /// <param name="secondPolynom"> The second polynomial</param>
+        /// <returns> New polynomial which is multiplication of two polynomials </returns>
+        public static Polynomial operator *(Polynomial firstPolynom, Polynomial secondPolynom)
         {
-            int countOfMembersToDelete = 0;
-            int indexOfFirstNotZero = 0;
-
-            for(int i =0;i<coefs.Length;i++)
-                if(coefs[i] == 0)
-                {
-                    countOfMembersToDelete++;
-                }
-                else
-                {
-                    indexOfFirstNotZero = i;
-                    break;
-                }
-
-            double[] newCoefs = new double[coefs.Length - countOfMembersToDelete];
-
-            for (int i = 0; i < newCoefs.Length; i++, indexOfFirstNotZero++)
-                newCoefs[i] = coefs[indexOfFirstNotZero];
-
-            return newCoefs;
-        }
-
-        /// <summary>
-        /// Multiply one polynom with another
-        /// </summary>
-        /// <param name="firstPolinom"> The first polynom</param>
-        /// <param name="secondPolinom"> The second polynom</param>
-        /// <returns> New polynom which is multiplication of two polynoms </returns>
-        public static Polynomial operator *(Polynomial firstPolinom, Polynomial secondPolinom)
-        {
-            if (firstPolinom == null && secondPolinom == null)
+            if (firstPolynom == null && secondPolynom == null)
+            {
                 return null;
+            }
 
-            if (firstPolinom == null)
-                return secondPolinom;
+            if (firstPolynom == null)
+            {
+                return secondPolynom;
+            }
 
-            if (secondPolinom == null)
-                return firstPolinom;
+            if (secondPolynom == null)
+            {
+                return firstPolynom;
+            }
 
-            double[] newCoefs = new double[firstPolinom.Length + secondPolinom.Length - 1];
+            double[] newCoefs = new double[firstPolynom.Length + secondPolynom.Length - 1];
 
-            for (int i = 0; i < firstPolinom.Length; i++)
-                for (int j = 0; j < secondPolinom.Length; j++)
-                    newCoefs[i + j] += firstPolinom[i] * secondPolinom[j];
+            for (int i = 0; i < firstPolynom.Length; i++)
+            {
+                for (int j = 0; j < secondPolynom.Length; j++)
+                {
+                    newCoefs[i + j] += firstPolynom[i] * secondPolynom[j];
+                }
+            }
 
             return new Polynomial(newCoefs);
         }
 
-        /// <summary>
-        /// Compare two polynoms.
-        /// </summary>
-        /// <param name="firstPolynom">The first polinom</param>
-        /// <param name="secondPolynom">The second polinom</param>
-        /// <returns>Result of comparison of two polinoms</returns>
+        /// <summary>Compare two polynomials</summary>
+        /// <param name="firstPolynom">The first polynomial</param>
+        /// <param name="secondPolynom">The second polynomial</param>
+        /// <returns>Result of comparison of two polynomials</returns>
         public static bool operator ==(Polynomial firstPolynom, Polynomial secondPolynom)
         {
             if ((object)firstPolynom == null || (object)secondPolynom == null || firstPolynom.Length != secondPolynom.Length)
@@ -324,12 +222,10 @@ namespace NET.S._2019.Zhidenko._5
             return true;
         }
 
-        /// <summary>
-        /// Compare two polynoms.
-        /// </summary>
-        /// <param name="firstPolynom">The first polynom</param>
-        /// <param name="secondPolynom">The second polynom</param>
-        /// <returns>Result of comparison of two polinoms</returns>
+        /// <summary>Compare two polynomials</summary>
+        /// <param name="firstPolynom">The first polynomial</param>
+        /// <param name="secondPolynom">The second polynomial</param>
+        /// <returns>Result of comparison of two polynomials</returns>
         public static bool operator !=(Polynomial firstPolynom, Polynomial secondPolynom)
         {
             if (firstPolynom.Length != secondPolynom.Length || firstPolynom == null || secondPolynom == null)
@@ -346,6 +242,134 @@ namespace NET.S._2019.Zhidenko._5
             }
 
             return false;
+        }
+
+        /// <summary>Calculate result of polynomial with certain X</summary>
+        /// <param name="value"> Value of X</param>
+        /// <returns> Result of polynomial </returns>
+        public double GetResult(double value)
+        {
+            double result = 0;
+
+            for (int i = this.coefs.Length - 1; i >= 0; i--)
+            {
+                result += this.coefs[i] * Math.Pow(value, i);
+            }
+
+            return result;
+        }
+
+        /// <summary>Transform polynomial to a string</summary>
+        /// <returns> Polynomial string</returns>
+        public override string ToString()
+        {
+            string result = string.Empty;
+
+            for (int i = 0; i < this.coefs.Length; i++)
+            {
+                if (this.coefs[i] == 0)
+                {
+                    continue;
+                }
+
+                if (result == string.Empty)
+                {
+                    if (Math.Abs(this.coefs[i]) != 1)
+                    {
+                        result += this.coefs[0].ToString();
+                    }
+                    else if (this.coefs[i] == -1)
+                    {
+                        result += "-";
+                    }
+                }
+                else
+                {
+                    result += (this.coefs[i] > 0) ? " + " : " - ";
+
+                    if (Math.Abs(this.coefs[i]) != 1 || i == this.coefs.Length - 1)
+                    {
+                        result += Math.Abs(this.coefs[i]).ToString();
+                    }
+                }
+
+                if (i != this.coefs.Length - 2 && i != this.coefs.Length - 1)
+                {
+                    result += "x^" + (this.coefs.Length - i - 1).ToString();
+                }
+                else if (i == this.coefs.Length - 2)
+                {
+                    result += "x";
+                }
+            }
+
+            return result;
+        }
+
+        /// <summary>Hash function</summary>
+        /// <returns> Hash of polynomial</returns>
+        public override int GetHashCode()
+        {
+            return this.coefs.GetHashCode();
+        }
+
+        /// <summary>Compare two polynomials </summary>
+        /// <param name="obj"> Polynomial to compare</param>
+        /// <returns> Result of comparison </returns>
+        public override bool Equals(object obj)
+        {
+            Polynomial p = (Polynomial)obj;
+
+            if (p == null)
+            {
+                return false;
+            }
+
+            if (this.coefs.Length != p.Length)
+            {
+                return false;
+            }
+
+            for (int i = 0; i < this.coefs.Length; i++)
+            {
+                if (this.coefs[i] != p[i])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        /// <summary>Delete unnecessary members of polynomial</summary>
+        /// <param name="coefs">Initial coefficients</param>
+        /// <returns>Coefficients without 0 at the beginning</returns>
+        private static double[] DeleteZeros(double[] coefs)
+        {
+            int countOfMembersToDelete = 0;
+            int indexOfFirstNotZero = 0;
+
+            for (int i = 0; i < coefs.Length; i++)
+            {
+                if (coefs[i] == 0)
+                {
+                    countOfMembersToDelete++;
+                }
+                else
+                {
+                    indexOfFirstNotZero = i;
+                    break;
+                }
+            }
+
+            double[] newCoefs = new double[coefs.Length - countOfMembersToDelete];
+
+            for (int i = 0; i < newCoefs.Length; i++, indexOfFirstNotZero++)
+            {
+                newCoefs[i] = coefs[indexOfFirstNotZero];
+            }
+
+            return newCoefs;
         }
     }
 }
