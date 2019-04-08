a9ed4f986a0967ab8f2b521cb1421b161b64d0b6
Andrei Zhidenko <zhidenko_1999@mail.ru>
2019-04-08 05:14:11 +0300

    Implementation of class Book

---
+++ NET.S.2019.Zhidenko.8/Book/Book.cs
@@ -0,0 +1,219 @@
+﻿using System;
+
+namespace NET.S._2019.Zhidenko._8
+{
+    public class Book: IEquatable<Book>, IComparable, IComparable<Book>
+    {
+        private string isbn;
+        private string name;
+        private string author;
+        private int pageCount;
+        private int price;
+        private int publicationYear;
+        private string publisher;
+
+        public Book(string isbn, string name, string author, string publisher, int publicationYear, int pageCount, int price)
+        {
+            Isbn = isbn;
+            Name = name;
+            Author = author;
+            Publisher = publisher;
+            PublicationYear = publicationYear;
+            PageCount = pageCount;
+            Price = price;
+        }
+
+        public string Isbn
+        {
+            get
+            {
+                return isbn;
+            }
+
+            set
+            {
+                if(value == null || value == string.Empty)
+                {
+                    throw new ArgumentException("Isbn can't be null or empty!");
+                }
+
+                isbn = value;
+            }
+        }
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+
+            set
+            {
+                if (value == null || value == string.Empty)
+                {
+                    throw new ArgumentException("Name can't be null or empty!");
+                }
+
+                name = value;
+            }
+        }
+
+        public string Author
+        {
+            get
+            {
+                return author;
+            }
+
+            set
+            {
+                if (value == null || value == string.Empty)
+                {
+                    throw new ArgumentException("Author can't be null or empty!");
+                }
+
+                author = value;
+            }
+        }
+
+        public string Publisher
+        {
+            get
+            {
+                return publisher;
+            }
+
+            set
+            {
+                if (value == null || value == string.Empty)
+                {
+                    throw new ArgumentException("Publisher can't be null or empty!");
+                }
+
+                publisher = value;
+            }
+        }
+
+        public int PageCount
+        {
+            get
+            {
+                return pageCount;
+            }
+
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Count of page can't be 0 or less!");
+                }
+
+                pageCount = value;
+            }
+        }
+
+        public int Price
+        {
+            get
+            {
+                return price;
+            }
+
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Price can't be 0 or less!");
+                }
+
+                price = value;
+            }
+        }
+
+        public int PublicationYear
+        {
+            get
+            {
+                return publicationYear;
+            }
+
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Year of publication can't be 0 or less!");
+                }
+
+                publicationYear = value;
+            }
+        }
+
+        public override string ToString()
+        {
+            return "Isbn: " + Isbn + ", Name: " + Name + ", Author: " + Author + ", Publisher: " +
+                Publisher + ", Year of publication: " + PublicationYear.ToString() + ", Count of page: " + 
+                PageCount.ToString() + ", Price: " + Price.ToString();
+        }
+
+        public override int GetHashCode()
+        {
+            var hashCode = 352033288;
+
+            hashCode = hashCode * -1521134295 + Isbn.GetHashCode();
+            hashCode = hashCode * -1521134295 + Name.GetHashCode();
+            hashCode = hashCode * -1521134295 + Author.GetHashCode();
+            hashCode = hashCode * -1521134295 + Publisher.GetHashCode();
+            hashCode = hashCode * -1521134295 + PublicationYear.GetHashCode();
+            hashCode = hashCode * -1521134295 + Price.GetHashCode();
+            hashCode = hashCode * -1521134295 + PageCount.GetHashCode();
+
+            return hashCode;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return Equals(obj as Book);
+        }
+
+        public bool Equals(Book book)
+        {
+            if (book == null)
+            {
+                return false;
+            }
+
+            if(Isbn == book.Isbn && Name == book.Name && Author == book.Author && Publisher == book.Publisher
+                && PublicationYear == book.PublicationYear && Price == book.Price && PageCount == book.PageCount)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public int CompareTo(Book book)
+        {
+            if(book == null)
+            {
+                return 1;
+            }
+
+            if(string.Compare(Name, book.Name) == 0 && string.Compare(Author, book.Author) == 0)
+            {
+                return 0;
+            }
+            else
+            {
+                return 1;
+            }
+        }
+
+        public int CompareTo(object obj)
+        {
+            return CompareTo(obj as Book);
+        }
+    }
+}
