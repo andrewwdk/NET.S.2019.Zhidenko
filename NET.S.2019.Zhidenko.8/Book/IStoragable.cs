019ca2639ff26842bfaba466afacd3db4203117e
Andrei Zhidenko <zhidenko_1999@mail.ru>
2019-04-10 06:10:13 +0300

    Add IStoragable interface

---
+++ NET.S.2019.Zhidenko.8/Book/IStoragable.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NET.S._2019.Zhidenko._8
+{
+    interface IStoragable
+    {
+        void PutBooksIntoStorage(List<Book> list);
+        List<Book> GetBooksFromStorage();
+    }
+}
