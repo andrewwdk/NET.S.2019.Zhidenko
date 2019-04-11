834b5f44e34661ba7309c365447d96a6e7ee91d1
Andrei Zhidenko <zhidenko_1999@mail.ru>
2019-04-12 02:55:16 +0300

    Add console application for testing

---
+++ NET.S.2019.Zhidenko.8/ConsoleApplicationForBankAccounts/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BankAccounts;
+using BankAccounts.AccountTypes;
+using BankAccounts.BonusTypes;
+
+namespace ConsoleApplicationForBankAccounts
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var account = new Account(1, "Andrei", "Zhidenko", new FileStorage("D:\\Курсы\\file.dat"), new BaseType(), new BasicBonus());
+
+            account.AddMoney(1000);
+
+            Account newAccount = account.Storage.Get();
+
+            Console.WriteLine("{0} {1} {2} {3} {4}", newAccount.Number, newAccount.Name, newAccount.Surname, newAccount.Money, newAccount.BonusPoints);
+
+            newAccount.WithdrawMoney(100);
+
+            Console.WriteLine("{0} {1} {2} {3} {4}", newAccount.Number, newAccount.Name, newAccount.Surname, newAccount.Money, newAccount.BonusPoints);
+
+            Console.ReadKey();
+        }
+    }
+}
