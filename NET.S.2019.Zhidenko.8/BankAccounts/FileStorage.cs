c63b4a0b1cbf9473a92730fb5edfd09f3208a367
Andrei Zhidenko <zhidenko_1999@mail.ru>
2019-04-12 02:43:49 +0300

    Add FileStorage class which implements (de-)cerialization methods

---
+++ NET.S.2019.Zhidenko.8/BankAccounts/FileStorage.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+
+namespace BankAccounts
+{
+    [Serializable]
+    public class FileStorage : IStoragable
+    {
+        private string storagePath;
+
+        public FileStorage(string path)
+        {
+            StoragePath = path;
+        }
+
+        public string StoragePath
+        {
+            get
+            {
+                return storagePath;
+            }
+
+            private set
+            {
+                if (value == null || value == string.Empty)
+                {
+                    throw new ArgumentException("Path to account storage can't be empty string or null");
+                }
+
+                storagePath = value;
+            }
+        }
+
+        /// <summary> Read account data from file. </summary>
+        /// <returns> Account. </returns>
+        public Account Get()
+        {
+            var bf = new BinaryFormatter();
+
+            using (var fs = File.Open(StoragePath, FileMode.Open))
+            {
+                return (Account)bf.Deserialize(fs);
+            }
+        }
+
+        /// <summary> Save account data into file. </summary>
+        /// <param name="account"> Account to save. </param>
+        public void Save(Account account)
+        {
+            var bf = new BinaryFormatter();
+
+            using (var fs = File.Open(StoragePath, FileMode.Create))
+            {
+                bf.Serialize(fs, account);
+            }
+        }
+    }
+}
