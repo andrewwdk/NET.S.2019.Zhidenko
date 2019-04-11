d8b8a0002cb84419a719a85a0bb7bf644a8853f1
Andrei Zhidenko <zhidenko_1999@mail.ru>
2019-04-11 20:01:52 +0300

    Add interface and classes for bonuses

---
+++ NET.S.2019.Zhidenko.8/BankAccounts/BonusTypes/BasicBonus.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BankAccounts.AccountTypes;
+
+namespace BankAccounts.BonusTypes
+{
+    public class BasicBonus : IBonus
+    {
+        public int AddMoney(AccountType accountType, decimal allMoney, decimal addedMoney)
+        {
+            return (int)(0.01 * accountType.BalanceCost * (double)allMoney + 0.05 * accountType.AddMoneyCost * (double)addedMoney);
+        }
+
+        public int WithdrawMoney(AccountType accountType, decimal allMoney, decimal withdrewMoney)
+        {
+            return (int)-(0.01 * accountType.BalanceCost * (double)allMoney + 0.01 * accountType.AddMoneyCost * (double)withdrewMoney);
+        }
+    }
+}
