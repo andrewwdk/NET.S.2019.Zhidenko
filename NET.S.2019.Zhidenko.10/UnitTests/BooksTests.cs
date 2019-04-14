933418211d4d2e568859d12da4e6259722a6a45b
Andrei Zhidenko <zhidenko_1999@mail.ru>
2019-04-15 02:55:41 +0300

    Add initial unit tests for book formatted presentation

---
+++ NET.S.2019.Zhidenko.10/UnitTests/BooksTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Books;
+using Books.Formats;
+using NUnit.Framework;
+
+namespace UnitTests
+{
+    [TestFixture]
+    public class BooksTests
+    {
+        [Test]
+        public void FormattedBookByName_ReturnsCorrectResult()
+        {
+            IFormattedBook b = new Book("1", "name1", "author1", "publisher1", 2019, 100, 10);
+
+            b = new NameDecorator(b);
+
+            Assert.AreEqual("Book: Name - name1", b.ToFormattedString());
+        }
+    }
+}
