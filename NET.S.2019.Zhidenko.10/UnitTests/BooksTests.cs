a3dca9ad65b90d3d0baabb7da4bb0d57b6e98be6
Andrei Zhidenko <zhidenko_1999@mail.ru>
2019-04-15 17:30:37 +0300

    Update unit tests for Books

--- NET.S.2019.Zhidenko.10/UnitTests/BooksTests.cs
+++ NET.S.2019.Zhidenko.10/UnitTests/BooksTests.cs
@@ -11,11 +11,28 @@ namespace UnitTests
         [Test]
         public void FormattedBookByName_ReturnsCorrectResult()
         {
-            IFormattedBook b = new Book("1", "name1", "author1", "publisher1", 2019, 100, 10);
+            Book b = new Book("1", "name1", "author1", "publisher1", 2019, 100, 10);
 
             b = new NameDecorator(b);
 
-            Assert.AreEqual("Book: Name - name1", b.ToFormattedString());
+            Assert.AreEqual("Book: Name - name1", b.ToString());
+        }
+
+        [Test]
+        public void FormattedBookByAllFields_ReturnsCorrectResult()
+        {
+            Book b = new Book("1", "name1", "author1", "publisher1", 2019, 100, 10);
+
+            b = new IsbnDecorator(b);
+            b = new NameDecorator(b);
+            b = new AuthorDecorator(b);
+            b = new PublisherDecorator(b);
+            b = new PublicationYearDecorator(b);
+            b = new PageCountDecorator(b);
+            b = new PriceDecorator(b);
+
+            Assert.AreEqual("Book: Isbn - 1 Name - name1 Author - author1 Publisher - publisher1 " +
+                "Year of publication - 2019 Count of page - 100 Price - 10", b.ToString());
         }
     }
 }
