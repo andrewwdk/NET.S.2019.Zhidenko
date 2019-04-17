8fc019da4d4d34349f0141a225b0744f2fd2b37b
Andrei Zhidenko <zhidenko_1999@mail.ru>
2019-04-17 14:33:56 +0300

    Add method for testing IFormatted

--- NET.S.2019.Zhidenko.10/UnitTests/BooksTests.cs
+++ NET.S.2019.Zhidenko.10/UnitTests/BooksTests.cs
@@ -12,16 +12,19 @@ namespace UnitTests
         public void FormattedBookByName_ReturnsCorrectResult()
         {
             Book b = new Book("1", "name1", "author1", "publisher1", 2019, 100, 10);
+            string expectedString = "Book: Name - name1";
 
             b = new NameDecorator(b);
 
-            Assert.AreEqual("Book: Name - name1", b.ToString());
+            Assert.AreEqual(expectedString, b.ToString());
         }
 
         [Test]
         public void FormattedBookByAllFields_ReturnsCorrectResult()
         {
             Book b = new Book("1", "name1", "author1", "publisher1", 2019, 100, 10);
+            string expectedString = "Book: Isbn - 1 Name - name1 Author - author1 Publisher - publisher1 " +
+                "Year of publication - 2019 Count of page - 100 Price - 10";
 
             b = new IsbnDecorator(b);
             b = new NameDecorator(b);
@@ -31,8 +34,19 @@ namespace UnitTests
             b = new PageCountDecorator(b);
             b = new PriceDecorator(b);
 
-            Assert.AreEqual("Book: Isbn - 1 Name - name1 Author - author1 Publisher - publisher1 " +
-                "Year of publication - 2019 Count of page - 100 Price - 10", b.ToString());
+            Assert.AreEqual(expectedString, b.ToString());
+        }
+
+        [Test]
+        public void FormattedToString_ReturnsCorrectResult()
+        {
+            Book b = new Book("1", "name1", "author1", "publisher1", 2019, 100, 10);
+            string expectedString = "Book: Isbn - 1 Name - name1 Author - author1 Publisher - publisher1 " +
+                "Year of publication - 2019 Count of page - 100 Price - 10";
+
+            string actualString = b.ToString("INAPYCp", null);
+
+            Assert.AreEqual(expectedString, actualString);
         }
     }
 }
