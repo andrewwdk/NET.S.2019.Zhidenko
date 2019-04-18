7d8914489da7f60a50c323710e9de77c34a4ced9
Andrei Zhidenko <zhidenko_1999@mail.ru>
2019-04-18 22:32:45 +0300

    Add tests for sorting jagged arrays

---
+++ NET.S.2019.Zhidenko.10/UnitTests/BubbleSortDelegateToInterfaceTests.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using MatrixSort;
+using NUnit.Framework;
+
+namespace UnitTests
+{
+    [TestFixture]
+    public class BubbleSortDelegateToInterfaceTests
+    {
+        [Test]
+        public void SortBySum_Decreasing_WithCorrectData_ReturnsCorrectResult()
+        {
+            int[][] actualArray = new int[][] { new int[] { 1, 2, 3 }, new int[] { 4, 5 }, new int[] { 10 } };
+            int[][] expectedArray = new int[][] { new int[] { 10 }, new int[] { 4, 5 }, new int[] { 1, 2, 3 } };
+            SortType type = SortType.decreasing;
+
+            BubbleSortDelegateToInterface.SortBySum(ref actualArray, type);
+
+            Assert.AreEqual(expectedArray, actualArray);
+        }
+
+        [Test]
+        public void SortBySum_Increasing_WithCorrectData_ReturnsCorrectResult()
+        {
+            int[][] expectedArray = new int[][] { new int[] { 1, 2, 3 }, new int[] { 4, 5 }, new int[] { 10 } };
+            int[][] actualArray = new int[][] { new int[] { 10 }, new int[] { 4, 5 }, new int[] { 1, 2, 3 } };
+            SortType type = SortType.increasing;
+
+            BubbleSortDelegateToInterface.SortBySum(ref actualArray, type);
+
+            Assert.AreEqual(expectedArray, actualArray);
+        }
+
+        [Test]
+        public void SortBySum_WithNullArray_ReturnArgumentNullException()
+        {
+            int[][] actualArray = null;
+            SortType type = SortType.increasing;
+
+            Assert.Throws<ArgumentNullException>(() => BubbleSortDelegateToInterface.SortBySum(ref actualArray, type));
+        }
+
+        [Test]
+        public void SortBySum_WithEmptyArray_ReturnArgumentException()
+        {
+            int[][] actualArray = new int[][] { };
+            SortType type = SortType.increasing;
+
+            Assert.Throws<ArgumentException>(() => BubbleSortDelegateToInterface.SortBySum(ref actualArray, type));
+        }
+
+        [Test]
+        public void SortByMaxElement_Decreasing_WithCorrectData_ReturnsCorrectResult()
+        {
+            int[][] actualArray = new int[][] { new int[] { 1, 2, 3 }, new int[] { 4, 5 }, new int[] { 10 } };
+            int[][] expectedArray = new int[][] { new int[] { 10 }, new int[] { 4, 5 }, new int[] { 1, 2, 3 } };
+            SortType type = SortType.decreasing;
+
+            BubbleSortDelegateToInterface.SortByMaxElement(ref actualArray, type);
+
+            Assert.AreEqual(expectedArray, actualArray);
+        }
+
+        [Test]
+        public void SortByMaxElement_Increasing_WithCorrectData_ReturnsCorrectResult()
+        {
+            int[][] expectedArray = new int[][] { new int[] { 1, 2, 3 }, new int[] { 4, 5 }, new int[] { 10 } };
+            int[][] actualArray = new int[][] { new int[] { 10 }, new int[] { 4, 5 }, new int[] { 1, 2, 3 } };
+            SortType type = SortType.increasing;
+
+            BubbleSortDelegateToInterface.SortByMaxElement(ref actualArray, type);
+
+            Assert.AreEqual(expectedArray, actualArray);
+        }
+
+        [Test]
+        public void SortByMaxElement_WithNullArray_ReturnArgumentNullException()
+        {
+            int[][] actualArray = null;
+            SortType type = SortType.increasing;
+
+            Assert.Throws<ArgumentNullException>(() => BubbleSortDelegateToInterface.SortByMaxElement(ref actualArray, type));
+        }
+
+        [Test]
+        public void SortByMaxElement_WithEmptyArray_ReturnArgumentException()
+        {
+            int[][] actualArray = new int[][] { };
+            SortType type = SortType.increasing;
+
+            Assert.Throws<ArgumentException>(() => BubbleSortDelegateToInterface.SortByMaxElement(ref actualArray, type));
+        }
+
+        [Test]
+        public void SortByMinElement_Decreasing_WithCorrectData_ReturnsCorrectResult()
+        {
+            int[][] actualArray = new int[][] { new int[] { 1, 2, 3 }, new int[] { 4, 5 }, new int[] { 10 } };
+            int[][] expectedArray = new int[][] { new int[] { 10 }, new int[] { 4, 5 }, new int[] { 1, 2, 3 } };
+            SortType type = SortType.decreasing;
+
+            BubbleSortDelegateToInterface.SortByMinElement(ref actualArray, type);
+
+            Assert.AreEqual(expectedArray, actualArray);
+        }
+
+        [Test]
+        public void SortByMinElement_Increasing_WithCorrectData_ReturnsCorrectResult()
+        {
+            int[][] expectedArray = new int[][] { new int[] { 1, 2, 3 }, new int[] { 4, 5 }, new int[] { 10 } };
+            int[][] actualArray = new int[][] { new int[] { 10 }, new int[] { 4, 5 }, new int[] { 1, 2, 3 } };
+            SortType type = SortType.increasing;
+
+            BubbleSortDelegateToInterface.SortByMinElement(ref actualArray, type);
+
+            Assert.AreEqual(expectedArray, actualArray);
+        }
+
+        [Test]
+        public void SortByMinElement_WithNullArray_ReturnArgumentNullException()
+        {
+            int[][] actualArray = null;
+            SortType type = SortType.increasing;
+
+            Assert.Throws<ArgumentNullException>(() => BubbleSortDelegateToInterface.SortByMinElement(ref actualArray, type));
+        }
+
+        [Test]
+        public void SortByMinElement_WithEmptyArray_ReturnArgumentException()
+        {
+            int[][] actualArray = new int[][] { };
+            SortType type = SortType.increasing;
+
+            Assert.Throws<ArgumentException>(() => BubbleSortDelegateToInterface.SortByMinElement(ref actualArray, type));
+        }
+    }
+}
---
+++ NET.S.2019.Zhidenko.10/UnitTests/BubbleSortInterfaceToDelegateTests.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using MatrixSort;
+using NUnit.Framework;
+
+namespace UnitTests
+{
+    [TestFixture]
+    class BubbleSortInterfaceToDelegateTests
+    {
+        [Test]
+        public void SortBySum_Decreasing_WithCorrectData_ReturnsCorrectResult()
+        {
+            int[][] actualArray = new int[][] { new int[] { 1, 2, 3 }, new int[] { 4, 5 }, new int[] { 10 } };
+            int[][] expectedArray = new int[][] { new int[] { 10 }, new int[] { 4, 5 }, new int[] { 1, 2, 3 } };
+            SortType type = SortType.decreasing;
+
+            BubbleSortInterfaceToDelegate.SortBySum(ref actualArray, type);
+
+            Assert.AreEqual(expectedArray, actualArray);
+        }
+
+        [Test]
+        public void SortBySum_Increasing_WithCorrectData_ReturnsCorrectResult()
+        {
+            int[][] expectedArray = new int[][] { new int[] { 1, 2, 3 }, new int[] { 4, 5 }, new int[] { 10 } };
+            int[][] actualArray = new int[][] { new int[] { 10 }, new int[] { 4, 5 }, new int[] { 1, 2, 3 } };
+            SortType type = SortType.increasing;
+
+            BubbleSortInterfaceToDelegate.SortBySum(ref actualArray, type);
+
+            Assert.AreEqual(expectedArray, actualArray);
+        }
+
+        [Test]
+        public void SortBySum_WithNullArray_ReturnArgumentNullException()
+        {
+            int[][] actualArray = null;
+            SortType type = SortType.increasing;
+
+            Assert.Throws<ArgumentNullException>(() => BubbleSortInterfaceToDelegate.SortBySum(ref actualArray, type));
+        }
+
+        [Test]
+        public void SortBySum_WithEmptyArray_ReturnArgumentException()
+        {
+            int[][] actualArray = new int[][] { };
+            SortType type = SortType.increasing;
+
+            Assert.Throws<ArgumentException>(() => BubbleSortInterfaceToDelegate.SortBySum(ref actualArray, type));
+        }
+
+        [Test]
+        public void SortByMaxElement_Decreasing_WithCorrectData_ReturnsCorrectResult()
+        {
+            int[][] actualArray = new int[][] { new int[] { 1, 2, 3 }, new int[] { 4, 5 }, new int[] { 10 } };
+            int[][] expectedArray = new int[][] { new int[] { 10 }, new int[] { 4, 5 }, new int[] { 1, 2, 3 } };
+            SortType type = SortType.decreasing;
+
+            BubbleSortInterfaceToDelegate.SortByMaxElement(ref actualArray, type);
+
+            Assert.AreEqual(expectedArray, actualArray);
+        }
+
+        [Test]
+        public void SortByMaxElement_Increasing_WithCorrectData_ReturnsCorrectResult()
+        {
+            int[][] expectedArray = new int[][] { new int[] { 1, 2, 3 }, new int[] { 4, 5 }, new int[] { 10 } };
+            int[][] actualArray = new int[][] { new int[] { 10 }, new int[] { 4, 5 }, new int[] { 1, 2, 3 } };
+            SortType type = SortType.increasing;
+
+            BubbleSortInterfaceToDelegate.SortByMaxElement(ref actualArray, type);
+
+            Assert.AreEqual(expectedArray, actualArray);
+        }
+
+        [Test]
+        public void SortByMaxElement_WithNullArray_ReturnArgumentNullException()
+        {
+            int[][] actualArray = null;
+            SortType type = SortType.increasing;
+
+            Assert.Throws<ArgumentNullException>(() => BubbleSortInterfaceToDelegate.SortByMaxElement(ref actualArray, type));
+        }
+
+        [Test]
+        public void SortByMaxElement_WithEmptyArray_ReturnArgumentException()
+        {
+            int[][] actualArray = new int[][] { };
+            SortType type = SortType.increasing;
+
+            Assert.Throws<ArgumentException>(() => BubbleSortInterfaceToDelegate.SortByMaxElement(ref actualArray, type));
+        }
+
+        [Test]
+        public void SortByMinElement_Decreasing_WithCorrectData_ReturnsCorrectResult()
+        {
+            int[][] actualArray = new int[][] { new int[] { 1, 2, 3 }, new int[] { 4, 5 }, new int[] { 10 } };
+            int[][] expectedArray = new int[][] { new int[] { 10 }, new int[] { 4, 5 }, new int[] { 1, 2, 3 } };
+            SortType type = SortType.decreasing;
+
+            BubbleSortInterfaceToDelegate.SortByMinElement(ref actualArray, type);
+
+            Assert.AreEqual(expectedArray, actualArray);
+        }
+
+        [Test]
+        public void SortByMinElement_Increasing_WithCorrectData_ReturnsCorrectResult()
+        {
+            int[][] expectedArray = new int[][] { new int[] { 1, 2, 3 }, new int[] { 4, 5 }, new int[] { 10 } };
+            int[][] actualArray = new int[][] { new int[] { 10 }, new int[] { 4, 5 }, new int[] { 1, 2, 3 } };
+            SortType type = SortType.increasing;
+
+            BubbleSortInterfaceToDelegate.SortByMinElement(ref actualArray, type);
+
+            Assert.AreEqual(expectedArray, actualArray);
+        }
+
+        [Test]
+        public void SortByMinElement_WithNullArray_ReturnArgumentNullException()
+        {
+            int[][] actualArray = null;
+            SortType type = SortType.increasing;
+
+            Assert.Throws<ArgumentNullException>(() => BubbleSortInterfaceToDelegate.SortByMinElement(ref actualArray, type));
+        }
+
+        [Test]
+        public void SortByMinElement_WithEmptyArray_ReturnArgumentException()
+        {
+            int[][] actualArray = new int[][] { };
+            SortType type = SortType.increasing;
+
+            Assert.Throws<ArgumentException>(() => BubbleSortInterfaceToDelegate.SortByMinElement(ref actualArray, type));
+        }
+    }
+}
