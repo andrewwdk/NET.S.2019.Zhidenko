61bfb844ff02accb55bb4127e38d4853adb5d11f
Andrei Zhidenko <zhidenko_1999@mail.ru>
2019-04-19 05:29:46 +0300

    Add application for testing

---
+++ NET.S.2019.Zhidenko.11/ConsoleApplicationForRoulette/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Roulette;
+
+namespace ConsoleApplicationForRoulette
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var roulette = new Roulette.Roulette();
+            var logging = new RouletteLogging(roulette);
+
+            for(var i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Fell number is {0}", roulette.SpinRouletteWheel());
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
