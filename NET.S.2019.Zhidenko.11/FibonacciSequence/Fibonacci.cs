24ed9c4fbe38ab7e86aeba0c5cfa3abd249ec8f8
Andrei Zhidenko <zhidenko_1999@mail.ru>
2019-04-19 06:19:30 +0300

    Implement class which contains method to generate Fibonacci sequence

---
+++ NET.S.2019.Zhidenko.11/FibonacciSequence/Fibonacci.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FibonacciSequence
+{
+    public class Fibonacci
+    {
+        /// <summary> Generate Fibonacci sequence</summary>
+        /// <returns> Next Fibonacci number</returns>
+        public static IEnumerable<int> GetNext()
+        {
+            yield return 1;
+            yield return 1;
+
+            var lastNumber = 1;
+            var preLastNumber = 1;
+
+            for (;;)
+            {
+                var currentNumber = lastNumber + preLastNumber;
+                preLastNumber = lastNumber;
+                lastNumber = currentNumber;
+
+                yield return currentNumber;
+            }
+        }
+    }
+}
