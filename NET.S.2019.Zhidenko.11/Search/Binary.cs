b06a1f0f5b593753f9a554be5f880d65bc304db5
Andrei Zhidenko <zhidenko_1999@mail.ru>
2019-04-19 07:43:35 +0300

    Implement generic method for binary search

---
+++ NET.S.2019.Zhidenko.11/Search/Binary.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Search
+{
+    public class Binary
+    {
+        /// <summary> Search value in array by binary search algorithm</summary>
+        /// <param name="array"> Array </param>
+        /// <param name="searchedValue"> Value to find </param>
+        /// <param name="comparer"> Comparator </param>
+        /// <returns> Search result</returns>
+        public static bool BinarySearch<T>(T[] array, T searchedValue, Comparer<T> comparer)
+        {
+            if (array == null || searchedValue == null || comparer == null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            if (array.Length == 0)
+            {
+                throw new ArgumentException();
+            }
+
+            var bottom = 0;
+            var top = array.Length - 1;
+            int middle = top / 2;
+
+            while (top >= bottom)
+            {
+                var compareResult = comparer.Compare(array[middle], searchedValue);
+
+                if (compareResult == 0)
+                {
+                    return true;
+                }
+
+                if (compareResult == 1)
+                {
+                    top = middle - 1;
+                }
+                else
+                {
+                    bottom = middle + 1;
+                }
+
+                middle = (top + bottom) / 2;
+            }
+
+            return false;
+        }
+    }
+}
