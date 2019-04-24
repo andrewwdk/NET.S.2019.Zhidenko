1adf93213e70a2e128a01bcff532634a190f7688
Andrei Zhidenko <zhidenko_1999@mail.ru>
2019-04-24 20:01:56 +0300

    Add cosole application for timer

---
+++ NET.S.2019.Zhidenko.11/ConsoleApplicationForClock/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Clock;
+
+namespace ConsoleApplicationForClock
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Clock.Clock clock = new Clock.Clock();
+            Listener listener = new Listener(clock);
+
+            clock.StartTimer(10000);
+
+            Console.ReadKey();
+        }
+    }
+}
