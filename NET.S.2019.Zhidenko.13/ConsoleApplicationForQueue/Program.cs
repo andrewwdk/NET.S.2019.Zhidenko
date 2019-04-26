06e483a813c260dfe86759661962b78005c38f53
Andrei Zhidenko <zhidenko_1999@mail.ru>
2019-04-26 06:43:49 +0300

    Add console application for queue class

---
+++ NET.S.2019.Zhidenko.13/ConsoleApplicationForQueue/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Queue;
+
+namespace ConsoleApplicationForQueue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<int> queue = new Queue<int>(new int[] { 1, 2 });
+
+            queue.Enqueue(3);
+            queue.Enqueue(4);
+
+            Console.Write("Current elements: ");
+
+            foreach(int element in queue)
+            {
+                Console.Write("{0} ", element);
+            }
+
+            Console.WriteLine("\nDequeued element: {0}", queue.Dequeue());
+
+            Console.Write("Left elements: ");
+
+            foreach (int element in queue)
+            {
+                Console.Write("{0} ", element);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
