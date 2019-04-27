d6e843f4eeab2fc7249cb776e79401b4f8d5c46b
Andrei Zhidenko <zhidenko_1999@mail.ru>
2019-04-27 16:28:48 +0300

    Add class for arguments for element change event

---
+++ NET.S.2019.Zhidenko.13/Matrixes/MatrixChangeEventArgs.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Matrixes
+{
+    public class MatrixChangeEventArgs<T> : EventArgs
+    {
+        public MatrixChangeEventArgs(int i, int j, T element)
+        {
+            this.I = i;
+            this.J = j;
+            this.Element = element;
+        }
+
+        public int I { get; private set; }
+
+        public int J { get; private set; }
+
+        public T Element { get; private set; }
+    }
+}
